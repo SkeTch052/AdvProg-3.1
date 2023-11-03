636080d741c2f9d1786a82b2e0f512dcc82ddff8
SkeTch <sketch13rus@mail.ru>
2023-11-03 16:45:23 +0300

    Proj completed

--- Homework 3.1.cpp
+++ Homework 3.1.cpp
@@ -1,10 +1,51 @@
 ﻿#include <iostream>
 
 class smart_array {
-
+private:
+    int* array;
+    int count, size;
+public:
+    smart_array(int user_size) {
+      size = user_size;
+      count = 0;
+      array = new int[size];
+    }
+    void add_element(int elem) {
+        if (count >= size) {
+          throw std::exception("ERROR 1: The number of elements is greater than the number of elements for which memory is allocated");
+        }
+        else {
+          array[count] = elem;
+          count++;
+        }
+    }
+    int get_element(int index) {
+        if (index <= 0 || index > size) {
+          throw std::exception("ERROR 2: Wrong index");
+        }
+        else {
+          return array[index - 1];
+        }
+    }
+    ~smart_array() {
+        delete[] array;
+        count = 0;
+    }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    try {
+        smart_array arr(5);
+        arr.add_element(1);
+        arr.add_element(4);
+        arr.add_element(155);
+        arr.add_element(14);
+        arr.add_element(15);
+        std::cout << arr.get_element(1) << std::endl;
+    }
+    catch (const std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+    return 0;
 }
